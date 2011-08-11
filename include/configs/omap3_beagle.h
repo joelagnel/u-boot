42a52251d8afc29670cc5049cdcaa40759a511f5
Koen Kooi <k-kooi@ti.com>
2011-08-11 00:39:28 -0500

    beagle: pass expansionboard name in bootargs
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>
    Signed-off-by: Joel A Fernandes <agnel.joel@gmail.com>
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

--- include/configs/omap3_beagle.h
+++ include/configs/omap3_beagle.h
@@ -202,6 +202,7 @@
 	"usbtty=cdc_acm\0" \
 	"console=ttyS2,115200n8\0" \
 	"mpurate=auto\0" \
+	"buddy=none "\
 	"vram=12M\0" \
 	"dvimode=1024x768MR-16@60\0" \
 	"defaultdisplay=dvi\0" \
@@ -212,6 +213,7 @@
 	"nandrootfstype=jffs2\0" \
 	"mmcargs=setenv bootargs console=${console} " \
 		"mpurate=${mpurate} " \
+		"buddy=${buddy} "\
 		"vram=${vram} " \
 		"omapfb.mode=dvi:${dvimode} " \
 		"omapdss.def_disp=${defaultdisplay} " \
@@ -219,6 +221,7 @@
 		"rootfstype=${mmcrootfstype}\0" \
 	"nandargs=setenv bootargs console=${console} " \
 		"mpurate=${mpurate} " \
+		"buddy=${buddy} "\
 		"vram=${vram} " \
 		"omapfb.mode=dvi:${dvimode} " \
 		"omapdss.def_disp=${defaultdisplay} " \
