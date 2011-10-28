andreimironenko/u-boot-davinci

TI EMAC driver : Implement auto-negotiation, rmii support
This patch enables auto-negotiation on every open call.
Also, the speed,duplex,rmii control bits are programmed based
on auto-negn results

Signed-off-by: Sriramakrishnan <srk@ti.com>

drivers/net/davinci_emac.c
@@ -215,16 +215,42 @@ static int gen_get_link_speed(int phy_addr)
 static int gen_auto_negotiate(int phy_addr)
 {
 	u_int16_t	tmp;
+	u_int16_t	val;
+	unsigned int cntr = 0;
 
 	if (!davinci_eth_phy_read(phy_addr, PHY_BMCR, &tmp))
 		return(0);
+
+	val = tmp | PHY_BMCR_DPLX | PHY_BMCR_AUTON
+		| PHY_BMCR_100MB ;
+	davinci_eth_phy_write(phy_addr, PHY_BMCR, val);
+	davinci_eth_phy_read(phy_addr, PHY_BMCR, &val);
+
+	/* advertise 100 Full Duplex */
+	davinci_eth_phy_read(phy_addr,PHY_ANAR, &val);
+	val |= (PHY_ANLPAR_10 | PHY_ANLPAR_10FD | PHY_ANLPAR_TX
+		| PHY_ANLPAR_TXFD);
+	davinci_eth_phy_write(phy_addr,PHY_ANAR, val);
+	davinci_eth_phy_read(phy_addr,PHY_ANAR, &val);
+
+	davinci_eth_phy_read(phy_addr, PHY_BMCR, &tmp);
 
 	/* Restart Auto_negotiation  */
 	tmp |= PHY_BMCR_AUTON;
 	davinci_eth_phy_write(phy_addr, PHY_BMCR, tmp);
 
-	/*check AutoNegotiate complete */
-	udelay (10000);
+	/*check AutoNegotiate complete - it can take upto 3 secs*/
+	do{
+		udelay(40000);
+		cntr++;
+
+		if (davinci_eth_phy_read(phy_addr, PHY_BMSR, &tmp)){
+			if(tmp & PHY_BMSR_AUTN_COMP)
+				break;
+		}
+
+	}while(cntr < 250);
+
 	if (!davinci_eth_phy_read(phy_addr, PHY_BMSR, &tmp))
 		return(0);
 
@@ -256,6 +282,7 @@ static int davinci_eth_open(struct eth_device *dev, bd_t *bis)
 	dv_reg_p		addr;
 	u_int32_t		clkdiv, cnt;
 	volatile emac_desc	*rx_desc;
+	u_int16_t		lpa_val,val;
 
 	debug_emac("+ emac_open\n");
 
@@ -353,15 +380,30 @@ static int davinci_eth_open(struct eth_device *dev, bd_t *bis)
 	/* Enable ch 0 only */
 	adap_emac->RXUNICASTSET = 0x01;
 
-	/* Enable MII interface and Full duplex mode */
-	adap_emac->MACCONTROL = (EMAC_MACCONTROL_MIIEN_ENABLE | EMAC_MACCONTROL_FULLDUPLEX_ENABLE);
-
 	/* Init MDIO & get link state */
 	clkdiv = (EMAC_MDIO_BUS_FREQ / EMAC_MDIO_CLOCK_FREQ) - 1;
 	adap_mdio->CONTROL = ((clkdiv & 0xff) | MDIO_CONTROL_ENABLE | MDIO_CONTROL_FAULT);
 
-	if (!phy.get_link_speed(active_phy_addr))
+	if (!phy.auto_negotiate(active_phy_addr))
 		return(0);
+
+	davinci_eth_phy_read(active_phy_addr,PHY_ANLPAR,&lpa_val);
+	if (lpa_val & (PHY_ANLPAR_10FD | PHY_ANLPAR_TXFD) ) {
+		/* set EMAC for Full Duplex  */
+		adap_emac->MACCONTROL = EMAC_MACCONTROL_MIIEN_ENABLE |
+					 EMAC_MACCONTROL_FULLDUPLEX_ENABLE;
+	}else{
+		/*set EMAC for Half Duplex  */
+		adap_emac->MACCONTROL = EMAC_MACCONTROL_MIIEN_ENABLE;
+	}
+
+#ifdef CONFIG_DRIVER_TI_EMAC_USE_RMII
+	if (lpa_val & (PHY_ANLPAR_TXFD | PHY_ANLPAR_TX) ) {
+		adap_emac->MACCONTROL |= EMAC_MACCONTROL_RMIISPEED_100;
+	} else {
+		adap_emac->MACCONTROL &= ~EMAC_MACCONTROL_RMIISPEED_100;
+	}
+#endif
 
 	/* Start receive process */
 	adap_emac->RX0HDP = (u_int32_t)emac_rx_desc;

joelagnel/u-boot

Signed-off-by: Tom Rini <trini@ti.com>

drivers/net/davinci_emac.c
@@ -291,9 +291,22 @@ static int gen_get_link_speed(int phy_addr)
 static int gen_auto_negotiate(int phy_addr)
 {
 	u_int16_t	tmp;
+	u_int16_t       val;
 
 	if (!davinci_eth_phy_read(phy_addr, MII_BMCR, &tmp))
 		return(0);
+
+	val = tmp | BMCR_FULLDPLX | BMCR_ANENABLE | BMCR_SPEED100;
+	davinci_eth_phy_write(phy_addr, MII_BMCR, val);
+	davinci_eth_phy_read(phy_addr, MII_BMCR, &val);
+
+	/* advertise 100 Full Duplex */
+	davinci_eth_phy_read(phy_addr, MII_ADVERTISE, &val);
+	val |= (LPA_10HALF | LPA_10FULL | LPA_100HALF | LPA_100FULL);
+	davinci_eth_phy_write(phy_addr, MII_ADVERTISE, val);
+	davinci_eth_phy_read(phy_addr, MII_ADVERTISE, &val);
+
+	davinci_eth_phy_read(phy_addr, MII_BMCR, &tmp);
 
 	/* Restart Auto_negotiation  */
 	tmp |= BMCR_ANENABLE;
@@ -346,8 +359,9 @@ static void  __attribute__((unused)) davinci_eth_gigabit_enable(void)
 static int davinci_eth_open(struct eth_device *dev, bd_t *bis)
 {
 	dv_reg_p		addr;
-	u_int32_t		clkdiv, cnt;
+	u_int32_t		clkdiv, cnt, regval;
 	volatile emac_desc	*rx_desc;
+	uint16_t 		lpa_val;
 
 	debug_emac("+ emac_open\n");
 
@@ -426,18 +440,6 @@ static int davinci_eth_open(struct eth_device *dev, bd_t *bis)
 	/* Enable ch 0 only */
 	writel(1, &adap_emac->RXUNICASTSET);
 
-	/* Enable MII interface and Full duplex mode */
-#ifdef CONFIG_SOC_DA8XX
-	writel((EMAC_MACCONTROL_MIIEN_ENABLE |
-		EMAC_MACCONTROL_FULLDUPLEX_ENABLE |
-		EMAC_MACCONTROL_RMIISPEED_100),
-	       &adap_emac->MACCONTROL);
-#else
-	writel((EMAC_MACCONTROL_MIIEN_ENABLE |
-		EMAC_MACCONTROL_FULLDUPLEX_ENABLE),
-	       &adap_emac->MACCONTROL);
-#endif
-
 	/* Init MDIO & get link state */
 	clkdiv = (EMAC_MDIO_BUS_FREQ / EMAC_MDIO_CLOCK_FREQ) - 1;
 	writel((clkdiv & 0xff) | MDIO_CONTROL_ENABLE | MDIO_CONTROL_FAULT,
@@ -446,8 +448,30 @@ static int davinci_eth_open(struct eth_device *dev, bd_t *bis)
 	/* We need to wait for MDIO to start */
 	udelay(1000);
 
-	if (!phy.get_link_speed(active_phy_addr))
+	if (!phy.auto_negotiate(active_phy_addr))
 		return(0);
+
+	davinci_eth_phy_read(active_phy_addr, MII_LPA, &lpa_val);
+	if (lpa_val & (LPA_10FULL | LPA_100FULL) ) {
+		/* set EMAC for Full Duplex  */
+		writel((EMAC_MACCONTROL_MIIEN_ENABLE |
+			EMAC_MACCONTROL_FULLDUPLEX_ENABLE),
+			&adap_emac->MACCONTROL);
+	} else {
+		/*set EMAC for Half Duplex  */
+		writel(EMAC_MACCONTROL_MIIEN_ENABLE, &adap_emac->MACCONTROL);
+	}
+
+#ifdef CONFIG_DRIVER_TI_EMAC_USE_RMII
+	regval = readl(&adap_emac->MACCONTROL);
+	if (lpa_val & (LPA_100FULL | LPA_100HALF) ) {
+		writel((regval | EMAC_MACCONTROL_RMIISPEED_100),
+				&adap_emac->MACCONTROL);
+	} else {
+		writel((regval & (~EMAC_MACCONTROL_RMIISPEED_100)),
+			&adap_emac->MACCONTROL);
+	}
+#endif
 
 	emac_gigabit_enable();
 