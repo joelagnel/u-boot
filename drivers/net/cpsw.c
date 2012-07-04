d04833cbcffbc145c0d28c093bbe4d251b2aee93
Mugunthan V N <mugunthanvnm@ti.com>
2012-07-04 13:46:03 +0530

    drivers: net: cpsw: halt cpsw properly to stop receiving properly
    During cpsw driver halt, cpdma and cpsw must be stopped from processing
    network packets to ensure that hardware doesn't modify any DDR data where
    kernel may have its own data structures
    
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>
    Signed-off-by: Tom Rini <trini@ti.com>

--- drivers/net/cpsw.c
+++ drivers/net/cpsw.c
@@ -830,6 +830,16 @@ static int cpsw_init(struct eth_device *dev, bd_t *bis)
 static void cpsw_halt(struct eth_device *dev)
 {
 	struct cpsw_priv	*priv = dev->priv;
+
+	writel(0, priv->dma_regs + CPDMA_TXCONTROL);
+	writel(0, priv->dma_regs + CPDMA_RXCONTROL);
+
+	/* soft reset the controller and initialize priv */
+	soft_reset(&priv->regs->soft_reset);
+
+	/* clear dma state */
+	soft_reset(priv->dma_regs + CPDMA_SOFTRESET);
+
 	priv->data.control(0);
 }
 
